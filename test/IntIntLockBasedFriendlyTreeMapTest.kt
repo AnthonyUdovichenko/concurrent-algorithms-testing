ba0bda2a4be193e1ba6864c61a70c9679653ffe6
Anthony Udovichenko <antonyud2000@yandex.ru>
2021-08-17 16:42:30 +0300

    Add LockBasedFriendlyTreeMap<Int, Int> test (doesn't work)

---
+++ test/IntIntLockBasedFriendlyTreeMapTest.kt
@@ -0,0 +1,87 @@
+/**
+ * This test doesn't work yet.
+ */
+
+import LockBasedFriendlyTreeMap.LockBasedFriendlyTreeMap
+import org.jetbrains.kotlinx.lincheck.LoggingLevel
+import org.jetbrains.kotlinx.lincheck.annotations.Operation
+import org.jetbrains.kotlinx.lincheck.annotations.Param
+import org.jetbrains.kotlinx.lincheck.check
+import org.jetbrains.kotlinx.lincheck.paramgen.IntGen
+import org.jetbrains.kotlinx.lincheck.strategy.managed.modelchecking.ModelCheckingOptions
+import org.jetbrains.kotlinx.lincheck.strategy.stress.StressOptions
+import org.jetbrains.kotlinx.lincheck.verifier.VerifierState
+import org.junit.Test
+
+private const val MIN_KEY = 1
+private const val MAX_KEY = 8
+
+private const val MIN_VALUE = 1
+private const val MAX_VALUE = 10
+
+@Param.Params(
+    Param(name = "key", gen = IntGen::class, conf = "$MIN_KEY:$MAX_KEY"),
+    Param(name = "value", gen = IntGen::class, conf = "$MIN_VALUE:$MAX_VALUE")
+)
+class IntIntLockBasedFriendlyTreeMapTest: VerifierState() {
+    private val tree: LockBasedFriendlyTreeMap<Int, Int> = LockBasedFriendlyTreeMap()
+
+    @Operation
+    fun containsKey(@Param(name = "key") key: Int): Boolean = tree.containsKey(key)
+
+    @Operation
+    fun contains(@Param(name = "key") key: Int): Boolean = tree.contains(key)
+
+    @Operation
+    fun get(@Param(name = "key") key: Int): Int? = tree.get(key)
+
+    @Operation
+    fun remove(@Param(name = "key") key: Int): Int? = tree.remove(key)
+
+    @Operation
+    fun putIfAbsent(@Param(name = "key") key: Int, @Param(name = "value") value: Int): Int? =
+        tree.putIfAbsent(key, value)
+
+    @Operation(handleExceptionsAsResult = [NullPointerException::class])
+    fun numNodes(): Int = tree.numNodes()
+
+    @Operation
+    fun getBalance(): Int = tree.balance
+
+    @Operation
+    fun clear() = tree.clear()
+
+    @Operation
+    fun getStructMods(): Long = tree.structMods
+
+    /**
+     * Lincheck throws ClassCastException when executing this test.
+     */
+    @Test
+    fun runStressTest() = StressOptions()
+        .iterations(100)
+        .invocationsPerIteration(10_000)
+        .actorsBefore(10)
+        .actorsAfter(10)
+        .threads(3)
+        .actorsPerThread(4)
+        .logLevel(LoggingLevel.INFO)
+        .check(this::class.java)
+
+    /**
+     * This test doesn't work due to non-determinism in LockBasedFriendlyTreeMap.
+     */
+    /*@Test
+    fun runModelCheckingTest() = ModelCheckingOptions()
+        .iterations(100)
+        .invocationsPerIteration(10_000)
+        .actorsBefore(10)
+        .actorsAfter(10)
+        .threads(3)
+        .actorsPerThread(5)
+        .verboseTrace()
+        .logLevel(LoggingLevel.INFO)
+        .check(this::class.java)*/
+
+    override fun extractState() = (MIN_KEY..MAX_KEY).map { key -> tree.get(key) }
+}
